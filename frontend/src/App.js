b536412469885ef31c71e4d7c4a88aa0d420510d
Matias Aguilar <mac.bol2011@gmail.com>
2022-03-14 12:39:32 -0400

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>
+        Products List
+      </main>
+    </div>
+  );
+}
+
+export default App;
